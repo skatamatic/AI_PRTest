fe3e2c5ed85720e4bf6ad503811899efe4cf0263
kwesley <kyle.wesley@scopeapp.com>
2025-07-02 12:38:21 -0600

    Use consts for some logs

--- AIPRTest/DummyPaymentGateway.cs
+++ AIPRTest/DummyPaymentGateway.cs
@@ -1,5 +1,7 @@
 ﻿public class DummyPaymentGateway : IPaymentGateway
 {
+    private const string SuccessLog = $"SOLID: Payment successful.";
+
     public bool ProcessPayment(string customerId, decimal amount)
     {
         Console.WriteLine($"SOLID: Attempting payment of {amount:C} for customer {customerId}.");
